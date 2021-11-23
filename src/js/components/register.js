1dca2ec0519cbbed21e2703c256a134aaa4a0dfc
Ruchibrata Kundu <ruchibrata@rediffmail.com>
2021-11-22 21:13:05 -0500

    removing the validation from the therapist register page for the backend

--- src/js/components/register.js
+++ src/js/components/register.js
@@ -1,26 +1,40 @@
-function ValidationEvent() {
+function ValidationEvent(formId) {
   //clear out the last error message
-  console.log("call the validation");
+  console.log("call the validation for the form ", formId);
   document.getElementById("error").innerHTML = "";
 
-  // Storing Field Values In Variables
-  var fName = document.getElementById("register").fName;
-  var lName = document.getElementById("register").lName;
-  var email = document.getElementById("email");
-  var psw = document.getElementById("password");
-  var repsw = document.getElementById("repassword");
-  var phone = document.getElementById("phone");
-  var zip = document.getElementById("zip");
+  // Storing commomn Field Values In Variables from both register page
+  var fName = document.getElementById(formId).fName;
+  var lName = document.getElementById(formId).lName;
+  var email = document.getElementById(formId).email;
+  var psw = document.getElementById(formId).password;
+  var repsw = document.getElementById(formId).repassword;
+  var phone = document.getElementById(formId).phone;
+  var zip = document.getElementById(formId).zip;
 
-  // call the vaidate functions for corresponding fields
-  fNameValiadte(fName);
-  lNameValiadte(lName);
-  emailValidate(email);
-  pswValidate(psw);
-  repswMatch(psw, repsw);
-  phoneValidate(phone);
-  zipValidate(zip);
-  return false;
+  // Storing Field Values In Variables from Therapist register page
+  // var category = document.getElementById(formId).category;
+  var exp = document.getElementById(formId).exp;
+
+  // call the vaidate functions for corresponding fields and if all of them true rerun true
+
+  //for the user register form
+  if ((formId = "register")) {
+    if (
+      fNameValiadte(fName) &&
+      lNameValiadte(lName) &&
+      emailValidate(email) &&
+      pswValidate(psw) &&
+      repswMatch(psw, repsw) &&
+      phoneValidate(phone) &&
+      zipValidate(zip)
+    ) {
+      return true;
+    } else return false;
+  }
+
+  //   expValidate(exp);
+  // return false;
 }
 
 // functions for different fields validation
@@ -32,7 +46,7 @@ const fNameValiadte = (fName) => {
       " * First name should be minimum 2 characters long ";
     fName.focus();
     return false;
-  }
+  } else return true;
 };
 
 // LastName validation
@@ -42,7 +56,7 @@ const lNameValiadte = (lName) => {
       " * Last name should be minimum 2 characters long ";
     lName.focus();
     return false;
-  }
+  } else return true;
 };
 
 // Valiadate email for the format (strin@string.string);
@@ -52,7 +66,7 @@ const emailValidate = (email) => {
     document.getElementById("error").innerHTML = "  * Enter a valid email ";
     email.focus();
     return false;
-  }
+  } else return true;
 };
 
 // Validate password
@@ -62,7 +76,7 @@ const pswValidate = (psw) => {
       "  * Password should be minimum 6 characters long ";
     psw.focus();
     return false;
-  }
+  } else return true;
 };
 
 // Match password and repeat-paasword
@@ -72,7 +86,7 @@ const repswMatch = (psw, repsw) => {
       "  * Password should match with Repeat Password ";
     repsw.focus();
     return false;
-  }
+  } else return true;
 };
 
 // Valiadate Phone No as 10 numbers
@@ -83,7 +97,7 @@ const phoneValidate = (phone) => {
       "  * Phone No should be 10 numbers";
     phone.focus();
     return false;
-  }
+  } else return true;
 };
 
 // Valiadate Zipcode as only 5 numbers
@@ -94,5 +108,16 @@ const zipValidate = (zip) => {
       "  * Zip Code should be 5 numbers";
     zip.focus();
     return false;
-  }
+  } else return true;
+};
+
+// Valiadate Therapist experience as only numbers
+const expValidate = (exp) => {
+  var reg = /^[0-9]*$/;
+  if (reg.test(exp.value) === false) {
+    document.getElementById("error").innerHTML =
+      "  * Experience shpould be only numbers";
+    exp.focus();
+    return false;
+  } else return true;
 };
